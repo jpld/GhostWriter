4e6fc870cac1b52c9081c951c228744cda916f91
jpld <imjunpyo@gmail.com>
2010-03-11 23:40:50 -0800

    reformat and unify QCPlugIn template code

--- GhostWriterPlugIn.m
+++ GhostWriterPlugIn.m
@@ -6,18 +6,17 @@
 //  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 //
 
-/* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
-#import <OpenGL/CGLMacro.h>
-
 #import "GhostWriterPlugIn.h"
 
 #define	kQCPlugIn_Name				@"GhostWriter"
 #define	kQCPlugIn_Description		@"GhostWriter description"
 
+
 // WORKAROUND - naming violation for cocoa memory management
 @interface QCPlugIn(GhostWriterAdditions)
 - (QCPlugInViewController*)createViewController NS_RETURNS_RETAINED;
 @end
+
 
 @implementation GhostWriterPlugIn
 
@@ -26,133 +25,111 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 @dynamic inputFoo, outputBar;
 */
 
-+ (NSDictionary*) attributes
-{
++ (NSDictionary*)attributes {
 	/*
 	Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
 	*/
-	
+
 	return [NSDictionary dictionaryWithObjectsAndKeys:kQCPlugIn_Name, QCPlugInAttributeNameKey, kQCPlugIn_Description, QCPlugInAttributeDescriptionKey, nil];
 }
 
-+ (NSDictionary*) attributesForPropertyPortWithKey:(NSString*)key
-{
++ (NSDictionary*)attributesForPropertyPortWithKey:(NSString*)key {
 	/*
 	Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
 	*/
-	
+
 	return nil;
 }
 
-+ (QCPlugInExecutionMode) executionMode
-{
++ (QCPlugInExecutionMode)executionMode {
 	/*
 	Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
 	*/
-	
+
 	return kQCPlugInExecutionModeProcessor;
 }
 
-+ (QCPlugInTimeMode) timeMode
-{
++ (QCPlugInTimeMode)timeMode {
 	/*
 	Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
 	*/
-	
+
 	return kQCPlugInTimeModeNone;
 }
 
-- (id) init
-{
-	if(self = [super init]) {
-		/*
-		Allocate any permanent resource required by the plug-in.
-		*/
-	}
-	
-	return self;
-}
-
-- (void) finalize
-{
-	/*
-	Release any non garbage collected resources created in -init.
-	*/
-	
-	[super finalize];
-}
-
-- (void) dealloc
-{
-	/*
-	Release any resources created in -init.
-	*/
-	
-	[super dealloc];
-}
-
-+ (NSArray*) plugInKeys
-{
++ (NSArray*)plugInKeys {
 	/*
 	Return a list of the KVC keys corresponding to the internal settings of the plug-in.
 	*/
-	
+
 	return nil;
 }
 
-- (id) serializedValueForKey:(NSString*)key;
-{
+#pragma mark -
+
+- (id)init {
+    self = [super init];
+	if (self) {
+	}	
+	return self;
+}
+
+- (void)finalize {
+	[super finalize];
+}
+
+- (void)dealloc {
+	[super dealloc];
+}
+
+#pragma mark -
+
+- (id)serializedValueForKey:(NSString*)key {
 	/*
 	Provide custom serialization for the plug-in internal settings that are not values complying to the <NSCoding> protocol.
 	The return object must be nil or a PList compatible i.e. NSString, NSNumber, NSDate, NSData, NSArray or NSDictionary.
 	*/
-	
+
 	return [super serializedValueForKey:key];
 }
 
-- (void) setSerializedValue:(id)serializedValue forKey:(NSString*)key
-{
+- (void)setSerializedValue:(id)serializedValue forKey:(NSString*)key {
 	/*
 	Provide deserialization for the plug-in internal settings that were custom serialized in -serializedValueForKey.
 	Deserialize the value, then call [self setValue:value forKey:key] to set the corresponding internal setting of the plug-in instance to that deserialized value.
 	*/
-	
+
 	[super setSerializedValue:serializedValue forKey:key];
 }
 
-- (QCPlugInViewController*) createViewController
-{
+- (QCPlugInViewController*)createViewController {
 	/*
 	Return a new QCPlugInViewController to edit the internal settings of this plug-in instance.
 	You can return a subclass of QCPlugInViewController if necessary.
 	*/
-	
+
 	return [[QCPlugInViewController alloc] initWithPlugIn:self viewNibName:@"Settings"];
 }
 
-@end
+#pragma mark -
+#pragma mark EXECUTION
 
-@implementation GhostWriterPlugIn (Execution)
-
-- (BOOL) startExecution:(id<QCPlugInContext>)context
-{
+- (BOOL)startExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
 	Return NO in case of fatal failure (this will prevent rendering of the composition to start).
 	*/
-	
+
 	return YES;
 }
 
-- (void) enableExecution:(id<QCPlugInContext>)context
-{
+- (void)enableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
 	*/
 }
 
-- (BOOL) execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments
-{
+- (BOOL)execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments {
 	/*
 	Called by Quartz Composer whenever the plug-in instance needs to execute.
 	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
@@ -161,19 +138,17 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	The OpenGL context for rendering can be accessed and defined for CGL macros using:
 	CGLContextObj cgl_ctx = [context CGLContextObj];
 	*/
-	
+
 	return YES;
 }
 
-- (void) disableExecution:(id<QCPlugInContext>)context
-{
+- (void)disableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
 	*/
 }
 
-- (void) stopExecution:(id<QCPlugInContext>)context
-{
+- (void)stopExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
 	*/
